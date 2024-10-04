44580c222d5c3dbefc93a92d349a92f855717f24
imi-tat0r <admin@imi-tat0r.net>
2024-10-04 08:21:50 +0200

    chore: Update signature

--- TeleportFix/TeleportFix.cs
+++ TeleportFix/TeleportFix.cs
@@ -22,7 +22,7 @@ public class TeleportFixConfig : BasePluginConfig
 public class TeleportFix : BasePlugin, IPluginConfig<TeleportFixConfig>
 {
     public override string ModuleName => "HvH.gg - Teleport/Crasher Fix";
-    public override string ModuleVersion => "1.0.4";
+    public override string ModuleVersion => "1.0.6";
     public override string ModuleAuthor => "imi-tat0r";
     
     public TeleportFixConfig Config { get; set; } = new();
